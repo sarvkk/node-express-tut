1280194c1bd44002320083c733d652b350ba1d69
Sarvajit <sarvajit@Sprehas-MacBook-Air.local>
2024-09-08 17:25:32 +0545

    second

--- express3rdsept/evet.js
+++ express3rdsept/evet.js
@@ -2,8 +2,10 @@ const { Console } = require('console');
 const EventEmitter = require('events');
 const customEmitter = new EventEmitter
 
-customEmitter.on("res",()=>{
-   console.log(`Data received`)
+customEmitter.on("res",(name,id)=>{
+   console.log(`Data received ${name} and ${id}`)
 })
-
-customEmitter.emit('res')
+customEmitter.on('res',()=>{
+   console.log('Some other logic here')
+})
+customEmitter.emit('res','john',37)
